bdd08cd024f2831299ea0de8fa9c9779944fcbd3
JENNIFER GRACIAS <jenniferegracias@gmail.com>
2021-04-11 19:50:43 +0530

    Few changes

--- extension/js/taskscript.js
+++ extension/js/taskscript.js
@@ -1,9 +1,14 @@
+document.addEventListener("DOMContentLoaded",function(){
+    randomTask();
+    document.getElementById('idea-but').addEventListener("click",randomTask)});
+
+
 function randomTask() {
     var taskList = [
         {name:"take a walk!", description: "Check out some parks near you", img: "images/walk.png",link:"https://www.google.com/maps/search/parks+near+me/"},
         {name:"drink some water!", description: "Stay hydrated!", img: "images/water.png",link:"https://giphy.com/gifs/thirsty-drink-water-stay-hydrated-iIdijgFZtkssLeoJmO"},
         {name:"stretch!", description: "You've been sitting for a long time!", img: "images/stretch.png",link:"https://www.youtube.com/watch?v=JJAHGpe0AVU"},
-        {name:"doodle!", description: "Unleash your creativity", img: "images/doodle.png",link:"https://www.youtube.com/watch?v=JJAHGpe0AVU"},
+        {name:"doodle!", description: "Unleash your creativity", img: "images/doodle.png",link:"https://upliftingplay.com/"},
         {name:"write in your journal!", description: "Reflect on your day", img: "images/write.png",link:"https://penzu.com/"},
         {name:"listen to some music!", description: "Have a little dance party", img: "images/music.png",link:"https://www.youtube.com/watch?v=qNKcQYK7bBQ"},
         {name:"read a book!", description: "Get lost in a new story", img: "images/book.png",link:"https://www.readanybook.com/"},
